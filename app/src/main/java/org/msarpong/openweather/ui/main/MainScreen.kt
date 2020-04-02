ccf52b8f6a6986a7f022c176035abd3f90c9bcb3
Marcello S <msarpong1991@gmail.com>
2020-04-02 22:40:39 +0200

    Added main screen layout, viewmodel and style

--- app/src/main/java/org/msarpong/openweather/ui/main/MainScreen.kt
+++ app/src/main/java/org/msarpong/openweather/ui/main/MainScreen.kt
@@ -2,12 +2,84 @@ package org.msarpong.openweather.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import org.msarpong.openweather.R
+import org.msarpong.openweather.datamapping.WeatherResponse
+import kotlin.math.roundToInt
+
 
 class MainScreen : AppCompatActivity() {
 
+    private lateinit var viewModel: MainViewModel
+
+    private lateinit var progressBar: ProgressBar
+
+    private lateinit var cityWeather: TextView
+    private lateinit var dateWeather: TextView
+    private lateinit var tempWeather: TextView
+    private lateinit var descriptionWeather: TextView
+    private lateinit var tempMinMaxWeather: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_screen)
+
+        viewModel = ViewModelProviders.of(this)[MainViewModel::class.java]
+        setupViews()
     }
+
+    private fun setupViews() {
+        progressBar = findViewById(R.id.progressBar)
+        dateWeather = findViewById(R.id.date_textview)
+        tempWeather = findViewById(R.id.temp_textview)
+        descriptionWeather = findViewById(R.id.descrption_textview)
+        tempMinMaxWeather = findViewById(R.id.temp_min_max_textview)
+        cityWeather = findViewById(R.id.city_textview)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        viewModel.state.observe(this, Observer { state ->
+            when (state) {
+                is MainState.InProgress -> showProgress()
+                is MainState.Error -> {
+                    hideProgress()
+                    showError(state.error)
+                }
+                is MainState.Success -> {
+                    hideProgress()
+                    showWeather(state.weatherList)
+                }
+            }
+        })
+
+        viewModel.send(MainEvent.Load)
+    }
+
+    private fun showWeather(response: WeatherResponse) {
+        Log.d("GiphyActivity", "showGifs: $response")
+        cityWeather.text = response.name
+        tempWeather.text = response.main.temp.roundToInt().toString()+"°"
+        dateWeather.text = "Wednesday - 01 April"
+        descriptionWeather.text = response.weather[0].description.capitalize()
+        tempMinMaxWeather.text =response.main.tempMin.roundToInt().toString()+"° - "+response.main.tempMax.roundToInt().toString()+"°"
+    }
+
+    private fun showError(error: Throwable) {
+
+    }
+
+    private fun showProgress() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgress() {
+        progressBar.visibility = View.GONE
+    }
+
 }
--- app/src/main/java/org/msarpong/openweather/ui/main/MainViewModel.kt
+++ app/src/main/java/org/msarpong/openweather/ui/main/MainViewModel.kt
@@ -1,2 +1,52 @@
 package org.msarpong.openweather.ui.main
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import org.msarpong.openweather.datamapping.WeatherResponse
+import org.msarpong.openweather.service.WeatherReceiver
+import org.msarpong.openweather.service.WeatherResult
+import org.msarpong.openweather.service.WeatherService
+
+
+sealed class MainEvent {
+    object Load : MainEvent()
+}
+
+sealed class MainState {
+    object InProgress : MainState()
+    data class Success(val weatherList: WeatherResponse) : MainState()
+    data class Error(val error: Throwable) : MainState()
+}
+
+class MainViewModel : ViewModel() {
+    var state: MutableLiveData<MainState> = MutableLiveData()
+
+    private val weatherService = WeatherService()
+
+    init {
+        state.value = MainState.InProgress
+    }
+
+    fun send(event: MainEvent) {
+        when (event) {
+            is MainEvent.Load -> loadContent()
+        }
+    }
+
+    fun loadContent() {
+        try {
+            weatherService.getWeather(object : WeatherReceiver {
+                override fun receive(result: WeatherResult) {
+                    when (result) {
+                        is WeatherResult.Success -> state.value = MainState.Success(weatherList = result.weatherList)
+                        is WeatherResult.Error ->state.value = MainState.Error(error = result.error)
+                    }
+                }
+
+            })
+        } catch (exception: Throwable) {
+            Log.d("MainViewModel", "loadContent()")
+        }
+    }
+}
