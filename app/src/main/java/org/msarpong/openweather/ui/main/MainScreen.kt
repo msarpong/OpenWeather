3f3e88b641075676a49ebbe3178b6d8936798d13
Marcello S <msarpong1991@gmail.com>
2020-04-02 20:54:50 +0200

    Initial commit

---
+++ app/src/main/java/org/msarpong/openweather/ui/main/MainScreen.kt
@@ -0,0 +1,13 @@
+package org.msarpong.openweather.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.msarpong.openweather.R
+
+class MainScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/org/msarpong/openweather/ui/splash/SplashScreen.kt
@@ -0,0 +1,13 @@
+package org.msarpong.openweather.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.msarpong.openweather.R
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+    }
+}
