2f0f180acbb18647c46c384b93171bb05ffdd380
Marcello Sarpong <msarpong1991@gmail.com>
2020-04-19 00:07:03 +0200

    Add shared preferences

--- app/src/main/java/org/msarpong/openweather/ui/setting/SettingScreen.kt
+++ app/src/main/java/org/msarpong/openweather/ui/setting/SettingScreen.kt
@@ -1,13 +1,16 @@
 package org.msarpong.openweather.ui.setting
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 import org.msarpong.openweather.R
 import org.msarpong.openweather.ui.main.MainScreen
-
+import org.msarpong.openweather.utils.SHARED_PREFS
 
 class SettingScreen : AppCompatActivity() {
 
@@ -15,20 +18,36 @@ class SettingScreen : AppCompatActivity() {
 
     private lateinit var backutton: ImageButton
 
+    private lateinit var unitSwitch: Switch
+
+    private lateinit var sharedPrefs: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.setting_screen)
         viewModel = ViewModelProviders.of(this)[SettingViewModel::class.java]
 
+        sharedPrefs = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE)
         setupViews()
     }
 
     private fun setupViews() {
         backutton = findViewById(R.id.close_btn)
+
         backutton.setOnClickListener {
             val intent = Intent(this, MainScreen::class.java)
             startActivity(intent)
             finish()
+        }
+
+        unitSwitch = findViewById(R.id.switchUnit)
+
+        unitSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                viewModel.send(SettingEvent.Unit(true))
+            } else {
+                viewModel.send(SettingEvent.Unit(false))
+            }
         }
     }
 }
--- app/src/main/java/org/msarpong/openweather/ui/setting/SettingViewModel.kt
+++ app/src/main/java/org/msarpong/openweather/ui/setting/SettingViewModel.kt
@@ -1,18 +1,37 @@
 package org.msarpong.openweather.ui.setting
 
 import androidx.lifecycle.ViewModel
-import org.msarpong.openweather.datamapping.WeatherResponse
 
 sealed class SettingEvent {
-    object Load : SettingEvent()
+    data class Notifications(val state: Boolean) : SettingEvent()
+    data class Unit(val state: Boolean) : SettingEvent()
+    data class DarkTheme(val state: Boolean) : SettingEvent()
 }
 
 sealed class SettingState {
-    object InProgress : SettingState()
-    data class Success(val weatherList: WeatherResponse) : SettingState()
-    data class Error(val error: Throwable) : SettingState()
+    object NotificationsDisabled : SettingState()
+    object NotificationsEnabled : SettingState()
+    object DarkThemeDisabled : SettingState()
+    object DarkThemeEnabled : SettingState()
+
 }
 
-class SettingViewModel : ViewModel() {
 
+class SettingViewModel() : ViewModel() {
+
+    fun send(event: SettingEvent) {
+        when (event) {
+            is SettingEvent.Unit -> saveUnit(event.state)
+            is SettingEvent.Notifications -> saveUnit(event.state)
+            is SettingEvent.DarkTheme -> saveUnit(event.state)
+        }
+    }
+
+    private fun saveUnit(event: Boolean) {
+
+    }
+
+    private fun saveAnimations(event: Boolean) {
+
+    }
 }
